93f2ed71017a9539b0d088f6d54809b3130ccc32
pushsla <push2001sla@gmail.com>
2020-05-12 09:55:35 +0300

    (Internal) execCommand (to execute command and get code and stdout) and linuxColumns(to align text to table) functions

--- torodofi/logic.cpp
+++ torodofi/logic.cpp
@@ -1,6 +1,3 @@
-#include <string>
-#include <vector>
-
 #include "logic.hpp"
 
 using namespace std;
@@ -29,6 +26,35 @@ string joinString(vector<string> avector, string delimiter) {
   }
   result += avector[avector.size() - 1];
 
+  return result;
+}
+
+string linuxColumns(string astring, string delimiter) {
+  string cmd = "echo -e \"" + astring + "\" ";
+  cmd += "| column -s $'" + delimiter + "' -t";
+  types::returnstatus status = execCommand(cmd);
+  return status.output;
+}
+
+types::returnstatus execCommand(string acommand) {
+  char buffer[128];
+  types::returnstatus result;
+
+  // Open pipe to file
+  FILE *pipe = popen(acommand.c_str(), "r");
+  if (!pipe) {
+    // result.code = 404;
+    // return result;
+    printf("execCommand: string acommand: %s\n", acommand.c_str());
+    throw std::invalid_argument("Incorrect command or PIPE error");
+  }
+
+  while (!feof(pipe)) {
+    if (fgets(buffer, 128, pipe) != NULL)
+      result.output += buffer;
+  }
+
+  result.code = pclose(pipe);
   return result;
 }
 
--- torodofi/logic.hpp
+++ torodofi/logic.hpp
@@ -1,17 +1,23 @@
 #pragma once
 
+#include <stdexcept>
 #include <string>
 #include <vector>
+
+#include "types.hpp"
 
 namespace toro {
 namespace logic {
 
+// execute command and get return code and stdout both
+types::returnstatus execCommand(std::string acommand);
 // function to split string by delimiter
 std::vector<std::string> splitString(std::string astring,
                                      std::string delimiter = " ");
 // function to join vector of strings into one vector
 std::string joinString(std::vector<std::string> avector,
                        std::string delimiter = " ");
-
+// function to align strings separated by '\n' to a table
+std::string linuxColumns(std::string astring, std::string delimiter = "\t");
 } // namespace logic
 } // namespace toro
